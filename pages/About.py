0ec6c4cb8162c073a7d6159e82865ca1baac0d13
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2024-06-08 19:52:24 +0700

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -1,12 +1,24 @@
+import streamlit as st
 import pandas as pd
-import yfinance as yf
-from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
-import streamlit as st
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import yfinance as yf
 import plotly.express as px
-from datetime import datetime
 import plotly.graph_objects as go
+import tensorflow as tf
+import random
+
+# Set seed for reproducibility
+seed = 42
+np.random.seed(seed)
+tf.random.set_seed(seed)
+random.seed(seed)
+
+if "symbols_list" not in st.session_state:
+    st.session_state.symbols_list = None
 
 st.set_page_config(
     layout='wide',
@@ -16,120 +28,157 @@ st.set_page_config(
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-# Function to create LSTM model
-def create_LSTM_model(time_step, epochs, batch_size, optimizer):
-    # Step 1: Load the Data from Yahoo Finance
-    data = yf.download('KKGI.JK', start='2020-01-01', end=datetime.now())
+def visualize_training_data(data_train):
+    st.subheader('Training data Visualization')
+    st.line_chart(data_train)
 
-    # Step 2: Preprocess the Data
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    data_scaled = scaler.fit_transform(data[['Close']])
+def generate_sequences(data, n_lookback, n_forecast):
+    X = []
+    Y = []
 
-    # Splitting data into training and testing sets (80% train, 20% test)
-    train_size = int(len(data_scaled) * 0.8)
-    train, test = data_scaled[:train_size], data_scaled[train_size:]
+    for i in range(n_lookback, len(data) - n_forecast + 1):
+        X.append(data[i - n_lookback: i])
+        Y.append(data[i: i + n_forecast])
 
-    # Step 3: Create Dataset
-    def create_dataset(dataset, time_step=1):
-        dataX, dataY = [], []
-        for i in range(len(dataset) - time_step):
-            a = dataset[i:(i + time_step), 0]
-            dataX.append(a)
-            dataY.append(dataset[i + time_step, 0])
-        return pd.DataFrame(dataX), pd.Series(dataY)
+    X = np.array(X)
+    Y = np.array(Y)
+    return X, Y
 
-    X_train, y_train = create_dataset(train, time_step)
-    X_test, y_test = create_dataset(test, time_step)
+def evaluate_model(model, X_train, Y_train, scaler):
+    train_predictions = model.predict(X_train)
+    train_predictions = scaler.inverse_transform(train_predictions.reshape(-1, 1))
+    Y_train_inv = scaler.inverse_transform(Y_train.reshape(-1, 1))
 
-    # Reshape input to be [samples, time steps, features] which is required for LSTM
-    X_train = X_train.values.reshape(X_train.shape[0], X_train.shape[1], 1)
-    X_test = X_test.values.reshape(X_test.shape[0], X_test.shape[1], 1)
+    rmse = np.sqrt(mean_squared_error(Y_train_inv, train_predictions))
+    mae = mean_absolute_error(Y_train_inv, train_predictions)
+    mape = np.mean(np.abs((Y_train_inv - train_predictions) / Y_train_inv)) * 100
+    mse = mean_squared_error(Y_train_inv, train_predictions)
 
-    # Step 4: Build the LSTM Model
-    model = Sequential()
-    model.add(LSTM(50, return_sequences=True, input_shape=(time_step, 1)))
-    model.add(LSTM(50, return_sequences=False))
-    model.add(Dense(1))
-    model.compile(optimizer=optimizer, loss='mean_squared_error')
-
-    # Step 5: Train the Model
-    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=batch_size, verbose=0)
-
-    # Step 6: Make Predictions
-    train_predict = model.predict(X_train)
-    test_predict = model.predict(X_test)
-
-    # Inverse transform to get actual values
-    train_predict = scaler.inverse_transform(train_predict)
-    test_predict = scaler.inverse_transform(test_predict)
-
-    # Prepare data for saving to CSV
-    real_data = scaler.inverse_transform(data_scaled).flatten()
-    train_data = pd.Series(index=data.index, data=[float('NaN')] * len(data))
-    train_data[time_step:len(train_predict) + time_step] = train_predict.flatten()
-    test_data = pd.Series(index=data.index, data=[float('NaN')] * len(data))
-    test_data[len(train_predict) + (time_step * 2):len(data_scaled)] = test_predict.flatten()
-
-    df = pd.DataFrame({
-        'Date': data.index,
-        'Real': real_data,
-        'Train Predict': train_data.values,
-        'Test Predict': test_data.values,
-        'Open': data['Open'],
-        'High': data['High'],
-        'Low': data['Low'],
-        'Close': data['Close']
-    })
-
-    return model, df
-
+    return rmse, mae, mape, mse, train_predictions
 
 with st.form(key='params_form'):
-    st.markdown("""
-        <div style="display: flex; justify-content: center;">
-            <h2>Prediksi Data Real LSTM Saham KKGI.JK</h2>
-        </div>
-    """, unsafe_allow_html=True)
-    st.divider()
+        st.markdown('<p class="params_text">Forcasting Dengan LSTM</p>', unsafe_allow_html=True)
+        st.divider()
 
-    optimizers = ['adam', 'adamax', 'sgd', 'rmsprop']
-    optimizer = st.selectbox('Optimizer', optimizers, key='optimizer_selectbox')
+        optimizers = ['adam', 'adamax', 'sgd', 'rmsprop'] 
+        optimizer = st.selectbox('Optimizer', optimizers, key='symbol_selectbox')
+        
+        n_lookback, n_forecast = st.columns(2)
+        with n_lookback:
+            n_lookback = st.number_input('Lookback', min_value=1, max_value=500, value=164, step=1)
+        with n_forecast:
+            n_forecast = st.number_input('Forecast', min_value=10, max_value=730, value=365, step=1, key='period_no_input')
+        
+        epochs, batch_size = st.columns(2)
+        with epochs:
+            epochs = st.number_input('Epochs', min_value=1, value=200)
+        with batch_size:
+             batch_size = st.number_input('Batch Size', min_value=1, value=32)
 
-    time_step = st.number_input('Time Step', min_value=1, max_value=500, value=30, step=1)
-
-    epochs, batch_size = st.columns(2)
-    with epochs:
-        epochs_value = st.number_input('Epochs', min_value=1, value=10)
-    with batch_size:
-        batch_size_value = st.number_input('Batch Size', min_value=1, value=32)
-
-    st.markdown('')
-    train_button = st.form_submit_button('Train Model')
-    st.markdown('')
+        st.markdown('')
+        train_button = st.form_submit_button('Train Model')
+        st.markdown('')
 
 if train_button:
-    # Create Model
-    model, df = create_LSTM_model(time_step, epochs_value, batch_size_value, optimizer)
+    data = yf.download(tickers='KKGI.JK', period='4y')
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    y = data['Close'].values.reshape(-1, 1)
+    y_scaled = scaler.fit_transform(y)
 
-    z1, z2 = st.columns((8, 2.5))
-    with z1:
-        fig = px.line(df, x='Date', y=['Real', 'Train Predict', 'Test Predict'], title='Stock Price Prediction')
-        st.plotly_chart(fig)
+    X, Y = generate_sequences(y_scaled, n_lookback, n_forecast)
+    train_size = int(len(X) * 0.8)
+    X_train, X_test = X[:train_size], X[train_size:]
+    Y_train, Y_test = Y[:train_size], Y[train_size:]
 
-        # Area chart
-        fig_area = px.area(df, x='Date', y=['Real', 'Train Predict', 'Test Predict'], title='Area Chart of Stock Price Prediction')
-        st.plotly_chart(fig_area)
+    model = Sequential([
+        LSTM(units=50, return_sequences=True, input_shape=(n_lookback, 1)),
+        LSTM(units=50),
+        Dense(n_forecast)
+    ])
+    model.compile(optimizer=optimizer, loss='mean_squared_error')
+    
+    eval_data = []
+    for epoch in range(epochs):
+        model.fit(X_train, Y_train, epochs=1, batch_size=batch_size, verbose=0)
+        Y_test_pred = model.predict(X_test)
+        Y_test_pred_rescaled = scaler.inverse_transform(Y_test_pred.reshape(-1, 1)).reshape(Y_test_pred.shape)
+        Y_test_rescaled = scaler.inverse_transform(Y_test.reshape(-1, 1)).reshape(Y_test.shape)
 
-        # Line chart with scatter plot
-        fig_combined = go.Figure()
-        fig_combined.add_trace(go.Scatter(x=df['Date'], y=df['Real'], mode='lines', name='Real'))
-        fig_combined.add_trace(go.Scatter(x=df['Date'], y=df['Train Predict'], mode='markers', name='Train Predict', marker=dict(color='blue')))
-        fig_combined.add_trace(go.Scatter(x=df['Date'], y=df['Test Predict'], mode='markers', name='Test Predict', marker=dict(color='red')))
-        fig_combined.update_layout(title='Stock Price Prediction with Train and Test Points', xaxis_title='Date')
+        rmse = np.sqrt(mean_squared_error(Y_test_rescaled.flatten(), Y_test_pred_rescaled.flatten()))
+        mae = mean_absolute_error(Y_test_rescaled.flatten(), Y_test_pred_rescaled.flatten())
+        mse = mean_squared_error(Y_test_rescaled.flatten(), Y_test_pred_rescaled.flatten())
+        mape = np.mean(np.abs((Y_test_rescaled.flatten() - Y_test_pred_rescaled.flatten()) / Y_test_rescaled.flatten())) * 100
 
-    with z2:
-        # Filter data to show only rows where Test Predict is not NaN
-        filtered_df = df[df['Test Predict'].notna()]
-        st.write(filtered_df[['Date', 'Real', 'Train Predict', 'Test Predict']])
+        eval_data.append({
+            'Epochs': epoch + 1,
+            'Optimizer': optimizer,
+            'RMSE': rmse,
+            'MAE': mae,
+            'MSE': mse,
+            'MAPE': mape
+        })
+
+    last_sequence = y_scaled[-n_lookback:].reshape(1, n_lookback, 1)
+    Y_ = model.predict(last_sequence)
+    Y_ = scaler.inverse_transform(Y_.flatten().reshape(-1, 1)).flatten()
+
+    df_past = data[['Close']].reset_index()
+    df_past.rename(columns={'index': 'Date', 'Close': 'Actual'}, inplace=True)
+    df_past['Date'] = pd.to_datetime(df_past['Date'])
+    df_past['Forecast'] = np.nan
+    df_past['Forecast'].iloc[-1] = df_past['Actual'].iloc[-1]
+
+    df_future = pd.DataFrame(columns=['Date', 'Actual', 'Forecast'])
+    df_future['Date'] = pd.date_range(start=df_past['Date'].iloc[-1] + pd.Timedelta(days=1), periods=n_forecast)
+    df_future['Forecast'] = Y_
+    df_future['Actual'] = np.nan
+
+    results = pd.concat([df_past, df_future]).set_index('Date')
+    mean_value = df_past['Actual'].mean()
+    results['Characteristic'] = np.where(results['Actual'].fillna(results['Forecast']) >= mean_value, 'high', 'low')
 
     st.success('Model training completed!')
+
+    # Line chart of Actual vs Forecast
+    fig = px.line(results.reset_index(), x='Date', y=['Actual', 'Forecast'], title='Actual vs Forecast')
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.write(future_results[['Forecast']])
+
+    st.write(results.describe())
+
+
+    # Evaluasi model
+    rmse, mae, mape, mse, _ = evaluate_model(model, X, Y, scaler)
+
+    # Mengonversi nilai-nilai menjadi persentase
+    mape = f"{mape:.2f}"  # Dua angka di belakang koma untuk MAPE
+    rmse = f"{rmse:.2f}"  # Dua angka di belakang koma untuk RMSE
+    mae = f"{mae:.2f}"  # Dua angka di belakang koma untuk MAE
+    mse = f"{mse:.2f}"  # Dua angka di belakang koma untuk MSE
+
+    rmse_col, mae_col, mape_col, mse_col = st.columns(4)
+
+    with rmse_col:
+        with st.container(border=True):
+            st.markdown('<div class="box-shadow">', unsafe_allow_html=True)
+            st.markdown(f"**RMSE**: {rmse}")
+            st.markdown('</div>', unsafe_allow_html=True)
+
+    with mae_col:
+        with st.container(border=True):
+            st.markdown('<div class="box-shadow">', unsafe_allow_html=True)
+            st.markdown(f"**MAE**: {mae}")
+            st.markdown('</div>', unsafe_allow_html=True)
+
+    with mape_col:
+        with st.container(border=True):
+            st.markdown('<div class="box-shadow">', unsafe_allow_html=True)
+            st.markdown(f"**MAPE**: {mape}")
+            st.markdown('</div>', unsafe_allow_html=True)
+
+    with mse_col:
+        with st.container(border=True):
+            st.markdown('<div class="box-shadow">', unsafe_allow_html=True)
+            st.markdown(f"**MSE**: {mse}")
+            st.markdown('</div>', unsafe_allow_html=True)
