03c23dceb4323a4081b66a78741deb311eac2ef2
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2024-06-08 18:36:46 +0700

    Update 2_LSTM_PREDIKSI_DATA_REAL.py

--- pages/2_LSTM_PREDIKSI_DATA_REAL.py
+++ pages/2_LSTM_PREDIKSI_DATA_REAL.py
@@ -87,7 +87,11 @@ def create_LSTM_model(time_step, epochs, batch_size, optimizer):
 st.title('Stock Price Prediction with LSTM')
 
 with st.form(key='params_form'):
-    st.markdown('<p class="params_text">Prediksi Data Real LSTM Saham KKGI.JK </p>', unsafe_allow_html=True)
+    st.markdown("""
+        <div style="display: flex; justify-content: center;">
+            <h2>Prediksi Data Real LSTM Saham KKGI.JK</h2>
+        </div>
+    """, unsafe_allow_html=True)
     st.divider()
 
     optimizers = ['adam', 'adamax', 'sgd', 'rmsprop']
