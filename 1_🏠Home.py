bccba757cfd0d19697eedbfc0815b4a01d47284e
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2024-06-12 21:24:38 +0700

    Update 1_🏠Home.py

--- 1_🏠Home.py
+++ 1_🏠Home.py
@@ -30,7 +30,7 @@ def main():
     st.title('PREDIKSI ANALISIS LSTM PADA SAHAM KKGI.JK')
     # Fetch data from Yahoo Finance for KKGI.JK from 2021
     ticker = "KKGI.JK"
-    data = yf.download(tickers=ticker, period='3y')
+    data = yf.download(tickers=ticker, start='2021-01-01')
  
     def add_range_selector(fig):
         fig.update_layout(
